d66dede7c5b11ff438f19f3b002e38b2730d0c00
Aida Yrysbekova <aidiko20@gmail.com>
2019-07-07 19:33:28 -0400

    added game questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
-document.getElementById("start").addEventListener("click", myFunction);
-
-function myFunction() {
-  document.getElementById("start").innerHTML = "START THE GAME!";
-}
+$(document).ready(function(){
+    $("button").click(function(){
+      $("p").toggle();
+    });
+  });
