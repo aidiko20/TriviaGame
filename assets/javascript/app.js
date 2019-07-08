9320ab9f5085b0b32806b6b5b9d9d4c0af8b81e3
Aida Yrysbekova <aidiko20@gmail.com>
2019-07-07 23:20:33 -0400

    added timer and restart and results buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-    $("button").click(function(){
-      $("p").toggle();
-    });
-  });
+
+i = 60;
+function onTimer() {
+  document.getElementById('timer').innerHTML = i;
+  i--;
+  if (i < 0) {
+    alert('You lose!');
+  }
+  else {
+    setTimeout(onTimer, 1000);
+  }
+}
